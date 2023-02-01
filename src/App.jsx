d7e6a0f5cc0e40e447496b360de4152519345b95
jhechus <jess.osm1412@gmail.com>
2023-01-31 21:35:43 -0600

    Ageregando Los Primeros pasos para el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
+import Listadopacientes from "./components/Listadopacientes"
+
+
+function App() {
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+
+      <div className=" mt-12 md:flex">
+        <Formulario />
+        <Listadopacientes />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
