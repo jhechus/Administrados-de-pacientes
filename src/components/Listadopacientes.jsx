a496762aac345edf3495ee5c327f2ce21b94efdc
jhechus <jess.osm1412@gmail.com>
2023-02-03 12:33:23 -0600

    mostrando texto de forma condicional

--- src/components/Listadopacientes.jsx
+++ src/components/Listadopacientes.jsx
@@ -3,25 +3,43 @@ import Paciente from "./Paciente"
 function Listadopacientes({Pacientes}) {
 
 
-
   return (
     <div className=" md:w-1/2 lg:w-3/5 ">
-      <h2 className=" font-black text-3xl text-center">
-        Listado Pacientes
-      </h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Administra Tus {''}
-        <span className=" text-indigo-600 font-bold"> 
-          Pacientes y Tus citas
-        </span>
-      </p>
 
-      { Pacientes.map( (paciente) => (
-          <Paciente
-            key = {paciente.id}
-            paciente = {paciente}
-          />
-        ))}
+      {Pacientes && Pacientes.length ? (
+        <>
+          <h2 className=" font-black text-3xl text-center">
+            Listado Pacientes
+          </h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra Tus {''}
+            <span className=" text-indigo-600 font-bold"> 
+              Pacientes y Tus citas
+            </span>
+          </p>
+
+        { Pacientes.map( (paciente) => (
+            <Paciente
+              key = {paciente.id}
+              paciente = {paciente}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className=" font-black text-3xl text-center">
+            No hay pacientes
+          </h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Agrega un {''}
+            <span className=" text-indigo-600 font-bold"> 
+              Paciente
+            </span>
+          </p>
+        </>
+      )}
+
+      
 
       
  
